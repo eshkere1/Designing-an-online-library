837f81a9b8d4d352628a61ba18af24e1ad8e4226
Pavel <piv17032009@gmail.com>
2025-04-19 13:06:15 +0300

    Split books list by pages.

--- render_website.py
+++ render_website.py
@@ -1,4 +1,6 @@
 from livereload import Server, shell
+from more_itertools import chunked
+import os
 
 import json
 from http.server import HTTPServer, SimpleHTTPRequestHandler
@@ -9,16 +11,20 @@ with open("meta_data.json", "r", encoding="utf-8") as my_file:
 
 
 def on_reload():
-    env = Environment(
-        loader=FileSystemLoader('.'),
-        autoescape=select_autoescape(['html', 'xml'])
-    )
-    template = env.get_template('template.html')
-    rendered_page = template.render(
-        books=books
-    )
-    with open('index.html', 'w', encoding="utf8") as file:
-        file.write(rendered_page)
+    books_count = 10
+    download_books_pages = list(chunked(books, books_count))
+    os.makedirs("pages", exist_ok=True)
+    for number, books_page in enumerate(download_books_pages):
+        env = Environment(
+            loader=FileSystemLoader('.'),
+            autoescape=select_autoescape(['html', 'xml'])
+        )
+        template = env.get_template('template.html')
+        rendered_page = template.render(
+            books=books_page
+        )
+        with open(f'pages/index{number}.html', 'w', encoding="utf8") as file:
+            file.write(rendered_page)
 
 
 on_reload()
